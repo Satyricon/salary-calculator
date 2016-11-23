4032913b7ebc7e4c5b349ce85129b49e037ac28e
Yuri Barseghyan <yubarseg@gmail.com>
2016-11-23 12:52:53 -0800

    code refactoring: extracted some functions

--- src/main/scala/SalaryCalculator.scala
+++ src/main/scala/SalaryCalculator.scala
@@ -19,12 +19,18 @@ final case class PersonByMorningAndEvening(id: String,
 
 final case class DailySalary(id: String,
                              name: String,
-                             date: Date,
                              normalSalary: Double,
                              eveningSalary: Double,
                              overtime: Double)
 
+final case class DailyHours(normalQHours: Double,
+                            eveningQHours: Double,
+                            overtime25QHours: Double,
+                            overtime50QHours: Double,
+                            overtimeDoubleQHours: Double)
+
 object SalaryCalculator {
+  // hourly rates
   val dailyRate: Double = 3.75
   val eveningRate: Double = dailyRate + 1.15
   val overtime25Multiplier: Double = 1.25
@@ -37,9 +43,11 @@ object SalaryCalculator {
       System.exit(0)
     }
 
+    // input and output file paths are provided as arguments
     val inputFilePath = args(0)
     val outputFilePath = args(1)
 
+    // creating a spark session
     val sparkSession = SparkSession.builder
       .master("local")
       .appName("spark session example")
@@ -65,70 +73,8 @@ object SalaryCalculator {
     ).as[Person]
 
     // DataSet transformed to PersonByMorningAndEvening with aggregation by morning and evening
-    val personByMorningAndEveningDS = transformedTimesDS.map(row => {
-      val beginningOfADayT = beginningOfADay(row.startTime);
-      val sixOCLockMorning = getSixOCLock(row.startTime)
-      val sixOClockEvening = getSixOCLock(row.startTime, false)
-      val nextDaySixOClockMorning = if (getSixOCLock(row.endTime).after(sixOCLockMorning)) getSixOCLock(row.endTime) else getSixOCLock(row.endTime, true, true)
-      val nextDaySixOClockEvening = if (getSixOCLock(row.endTime, false).after(nextDaySixOClockMorning)) getSixOCLock(row.endTime, false) else getSixOCLock(row.endTime, false, true)
-
-      if (row.startTime.before(sixOCLockMorning) && (row.endTime.before(sixOCLockMorning) || row.endTime == sixOCLockMorning)) {
-        val timeDiffInQuarterHoursEveningBeforeDay = timeDifferenceInQuarterHours(row.endTime, row.startTime)
-
-        PersonByMorningAndEvening(row.id, row.name, beginningOfADayT, 0, timeDiffInQuarterHoursEveningBeforeDay, 0)
-      }
-      else if(row.startTime.before(sixOCLockMorning) && row.endTime.after(sixOCLockMorning) && (row.endTime.before(sixOClockEvening) || row.endTime == sixOClockEvening)) {
-        val timeDiffInQuarterHoursEveningBeforeDay = timeDifferenceInQuarterHours(sixOCLockMorning, row.startTime)
-        val timeDiffInQuarterHoursDay = timeDifferenceInQuarterHours(row.endTime, sixOCLockMorning)
-
-        PersonByMorningAndEvening(row.id, row.name, beginningOfADayT, timeDiffInQuarterHoursDay, timeDiffInQuarterHoursEveningBeforeDay, 0)
-      }
-      else if(row.startTime.before(sixOCLockMorning) && row.endTime.after(sixOClockEvening)) {
-        val timeDiffInQuarterHoursEveningBeforeDay = timeDifferenceInQuarterHours(sixOCLockMorning, row.startTime)
-        val timeDiffInQuarterHoursEveningAfterDay = timeDifferenceInQuarterHours(row.endTime, sixOClockEvening)
-        val timeDiffInQuarterHoursDay = timeDifferenceInQuarterHours(sixOClockEvening, sixOCLockMorning)
-
-        PersonByMorningAndEvening(row.id, row.name, beginningOfADayT, timeDiffInQuarterHoursDay, timeDiffInQuarterHoursEveningBeforeDay, timeDiffInQuarterHoursEveningAfterDay)
-      }
-      else if((row.startTime.after(sixOCLockMorning) || row.startTime == sixOCLockMorning) && row.startTime.before(sixOClockEvening) && (row.endTime.before(sixOClockEvening) || row.endTime == sixOClockEvening)) {
-        val timeDiffInQuarterHoursDay = timeDifferenceInQuarterHours(row.endTime, row.startTime)
-
-        PersonByMorningAndEvening(row.id, row.name, beginningOfADayT, timeDiffInQuarterHoursDay, 0, 0)
-      }
-      else if((row.startTime.after(sixOCLockMorning) || row.startTime == sixOCLockMorning) && row.startTime.before(sixOClockEvening) && row.endTime.after(sixOClockEvening) && (row.endTime.before(nextDaySixOClockMorning) || row.endTime == nextDaySixOClockMorning)) {
-        val timeDiffInQuarterHoursDay = timeDifferenceInQuarterHours(sixOClockEvening, row.startTime)
-        val timeDiffInQuarterHoursEveningAfterDay = timeDifferenceInQuarterHours(row.endTime, sixOClockEvening)
-
-        PersonByMorningAndEvening(row.id, row.name, beginningOfADayT, timeDiffInQuarterHoursDay, 0, timeDiffInQuarterHoursEveningAfterDay)
-      }
-      else if((row.startTime.after(sixOCLockMorning) || row.startTime == sixOCLockMorning) && row.startTime.before(sixOClockEvening) && row.endTime.after(sixOClockEvening) && row.endTime.after(nextDaySixOClockMorning)) {
-        val timeDiffInQuarterHoursDay = timeDifferenceInQuarterHours(sixOClockEvening, row.startTime) + timeDifferenceInQuarterHours(row.endTime, nextDaySixOClockMorning)
-        val timeDiffInQuarterHoursEveningAfterDay = timeDifferenceInQuarterHours(nextDaySixOClockMorning, sixOClockEvening)
-
-        PersonByMorningAndEvening(row.id, row.name, beginningOfADayT, timeDiffInQuarterHoursDay, 0, timeDiffInQuarterHoursEveningAfterDay)
-      }
-      else if((row.startTime.after(sixOClockEvening) || row.startTime == sixOClockEvening) && (row.endTime.before(nextDaySixOClockMorning) || row.endTime == nextDaySixOClockMorning)) {
-        val timeDiffInQuarterHoursEveningAfterDay = timeDifferenceInQuarterHours(row.endTime, row.startTime)
-
-        PersonByMorningAndEvening(row.id, row.name, beginningOfADayT, 0, 0, timeDiffInQuarterHoursEveningAfterDay)
-      }
-      else if((row.startTime.after(sixOClockEvening) || row.startTime == sixOClockEvening) && row.endTime.after(nextDaySixOClockMorning) && (row.endTime.before(nextDaySixOClockEvening) || row.endTime == nextDaySixOClockEvening)) {
-        val timeDiffInQuarterHoursDay = timeDifferenceInQuarterHours(row.endTime, nextDaySixOClockMorning)
-        val timeDiffInQuarterHoursEveningBeforeDay = timeDifferenceInQuarterHours(nextDaySixOClockMorning, row.startTime)
-
-        PersonByMorningAndEvening(row.id, row.name, beginningOfADayT, timeDiffInQuarterHoursDay, timeDiffInQuarterHoursEveningBeforeDay, 0)
-      }
-      else if((row.startTime.after(sixOClockEvening) || row.startTime == sixOClockEvening) && row.endTime.after(nextDaySixOClockEvening)) {
-        val timeDiffInQuarterHoursDay = timeDifferenceInQuarterHours(row.endTime, nextDaySixOClockMorning)
-        val timeDiffInQuarterHoursEveningBeforeDay = timeDifferenceInQuarterHours(nextDaySixOClockMorning, row.startTime)
-        val timeDiffInQuarterHoursEveningAfterDay = timeDifferenceInQuarterHours(row.endTime, nextDaySixOClockEvening)
-
-        PersonByMorningAndEvening(row.id, row.name, beginningOfADayT, timeDiffInQuarterHoursDay, timeDiffInQuarterHoursEveningBeforeDay, timeDiffInQuarterHoursEveningAfterDay)
-      }
-      else {
-        PersonByMorningAndEvening(row.id, row.name, beginningOfADayT, 0, 0, 0)
-      }
-    }).groupBy("id", "name", "date")
+    val personByMorningAndEveningDS = transformedTimesDS.map(getPersonByMorningAndEveningFromInterval)
+      .groupBy("id", "name", "date")
       .agg(
         sum("dayQHours").as("dayQHours"),
         sum("eveningQHoursBeforeDay").as("eveningQHoursBeforeDay"),
@@ -136,37 +82,30 @@ object SalaryCalculator {
       ).as[PersonByMorningAndEvening]
 
     // DataFrame contains salary portions by types (normal, evening and overtime)
-    val salaryByTypeDF = personByMorningAndEveningDS.map(row => {
-      val totalQHours = row.dayQHours + row.eveningQHoursBeforeDay + row.eveningQHoursAfterDay
-
-      if(totalQHours <= 32) {
-        DailySalary(row.id, row.name, new Date(row.date.getTime), row.dayQHours * dailyRate / 4, (row.eveningQHoursBeforeDay + row.eveningQHoursAfterDay) * eveningRate / 4, 0)
+    val salaryByTypeDF = personByMorningAndEveningDS.map(person => {
+      // if total working hours are less than 8 for the whole day we just count everything in as it is without overtimes
+      // otherwise we calculate how many normal hours, evening hours and overtime hours (by type) there should be
+      // for the whole day and calculate daily salary
+      if(person.dayQHours + person.eveningQHoursBeforeDay + person.eveningQHoursAfterDay <= 32) {
+        DailySalary(
+          person.id,
+          person.name,
+          person.dayQHours * dailyRate / 4,
+          (person.eveningQHoursBeforeDay + person.eveningQHoursAfterDay) * eveningRate / 4,
+          0
+        )
       }
       else {
-        val overtimeTotal = totalQHours - 32
-        val overtime25 = if (overtimeTotal > 8) 8 else overtimeTotal
-        val overtime50 = if (overtimeTotal > overtime25) {
-          if (overtimeTotal - overtime25 > 8) 8 else overtimeTotal - overtime25
-        } else 0
-        val overtimeDouble = if (overtimeTotal > overtime25 + overtime50) overtimeTotal - overtime25 - overtime50 else 0
-        val (dayQHours, eveningQHours): (Long, Long) = if(row.eveningQHoursBeforeDay == 0) {
-          if(row.dayQHours >= 32) (32, 0) else (row.dayQHours, 32 - row.dayQHours)
-        }
-        else {
-          if(row.dayQHours >= 32) (32 - row.eveningQHoursBeforeDay, row.eveningQHoursBeforeDay) else {
-            if(row.dayQHours + row.eveningQHoursBeforeDay >= 32) (32 - row.eveningQHoursBeforeDay, row.eveningQHoursBeforeDay) else (row.dayQHours, 32 - row.dayQHours)
-          }
-        }
+        val dailyHours: DailyHours = calculateDailyHours(person)
 
         DailySalary(
-          row.id,
-          row.name,
-          new Date(row.date.getTime),
-          dayQHours * dailyRate / 4,
-          eveningQHours * eveningRate / 4,
-          overtime25 * overtime25Multiplier * dailyRate / 4 +
-            overtime50 * overtime50Multiplier * dailyRate / 4 +
-            overtimeDouble * overtimeDoubleMultiplier * dailyRate / 4
+          person.id,
+          person.name,
+          dailyHours.normalQHours * dailyRate / 4,
+          dailyHours.eveningQHours * eveningRate / 4,
+          dailyHours.overtime25QHours * overtime25Multiplier * dailyRate / 4 +
+            dailyHours.overtime50QHours * overtime50Multiplier * dailyRate / 4 +
+            dailyHours.overtimeDoubleQHours * overtimeDoubleMultiplier * dailyRate / 4
         )
       }
     }).groupBy("id", "name").agg(
@@ -189,12 +128,94 @@ object SalaryCalculator {
     saveDfToCsv(finalDF, outputFilePath)
   }
 
-  // returns a Date for a given Timestamp
-  def beginningOfADay(t: Timestamp): Date = {
-    new Date(t.getTime)
+  // returns DailyHours constructed based on combination of normal, evening and total hours for a day
+  def calculateDailyHours(person: PersonByMorningAndEvening): DailyHours = {
+    val totalQHours = person.dayQHours + person.eveningQHoursBeforeDay + person.eveningQHoursAfterDay
+    val overtimeTotal = totalQHours - 32
+    val overtime25 = if (overtimeTotal > 8) 8 else overtimeTotal
+    val overtime50 = if (overtimeTotal > overtime25) {
+      if (overtimeTotal - overtime25 > 8) 8 else overtimeTotal - overtime25
+    } else 0
+    val overtimeDouble = if (overtimeTotal > overtime25 + overtime50) overtimeTotal - overtime25 - overtime50 else 0
+    val (dayQHours, eveningQHours): (Long, Long) = if(person.eveningQHoursBeforeDay == 0) {
+      if(person.dayQHours >= 32) (32, 0) else (person.dayQHours, 32 - person.dayQHours)
+    }
+    else {
+      if(person.dayQHours >= 32) (32 - person.eveningQHoursBeforeDay, person.eveningQHoursBeforeDay) else {
+        if(person.dayQHours + person.eveningQHoursBeforeDay >= 32) (32 - person.eveningQHoursBeforeDay, person.eveningQHoursBeforeDay) else (person.dayQHours, 32 - person.dayQHours)
+      }
+    }
+
+    DailyHours(dayQHours, eveningQHours, overtime25, overtime50, overtimeDouble)
   }
 
-  // return a new Timestamp corresponding to 18:00 of a same day as provided Timestamp
+  // returns PersonByMorningAndEvening constructed based on startTime and endTime under person
+  def getPersonByMorningAndEveningFromInterval(person: Person): PersonByMorningAndEvening = {
+    val beginningOfADay = new Date(person.startTime.getTime)
+    val sixOCLockMorning = getSixOCLock(person.startTime)
+    val sixOClockEvening = getSixOCLock(person.startTime, false)
+    val nextDaySixOClockMorning = getSixOCLock(person.startTime, true, true)
+    val nextDaySixOClockEvening = getSixOCLock(person.startTime, false, true)
+
+    if (person.startTime.before(sixOCLockMorning) && (person.endTime.before(sixOCLockMorning) || person.endTime == sixOCLockMorning)) {
+      val timeDiffInQuarterHoursEveningBeforeDay = timeDifferenceInQuarterHours(person.endTime, person.startTime)
+
+      PersonByMorningAndEvening(person.id, person.name, beginningOfADay, 0, timeDiffInQuarterHoursEveningBeforeDay, 0)
+    }
+    else if(person.startTime.before(sixOCLockMorning) && person.endTime.after(sixOCLockMorning) && (person.endTime.before(sixOClockEvening) || person.endTime == sixOClockEvening)) {
+      val timeDiffInQuarterHoursEveningBeforeDay = timeDifferenceInQuarterHours(sixOCLockMorning, person.startTime)
+      val timeDiffInQuarterHoursDay = timeDifferenceInQuarterHours(person.endTime, sixOCLockMorning)
+
+      PersonByMorningAndEvening(person.id, person.name, beginningOfADay, timeDiffInQuarterHoursDay, timeDiffInQuarterHoursEveningBeforeDay, 0)
+    }
+    else if(person.startTime.before(sixOCLockMorning) && person.endTime.after(sixOClockEvening)) {
+      val timeDiffInQuarterHoursEveningBeforeDay = timeDifferenceInQuarterHours(sixOCLockMorning, person.startTime)
+      val timeDiffInQuarterHoursEveningAfterDay = timeDifferenceInQuarterHours(person.endTime, sixOClockEvening)
+      val timeDiffInQuarterHoursDay = timeDifferenceInQuarterHours(sixOClockEvening, sixOCLockMorning)
+
+      PersonByMorningAndEvening(person.id, person.name, beginningOfADay, timeDiffInQuarterHoursDay, timeDiffInQuarterHoursEveningBeforeDay, timeDiffInQuarterHoursEveningAfterDay)
+    }
+    else if((person.startTime.after(sixOCLockMorning) || person.startTime == sixOCLockMorning) && person.startTime.before(sixOClockEvening) && (person.endTime.before(sixOClockEvening) || person.endTime == sixOClockEvening)) {
+      val timeDiffInQuarterHoursDay = timeDifferenceInQuarterHours(person.endTime, person.startTime)
+
+      PersonByMorningAndEvening(person.id, person.name, beginningOfADay, timeDiffInQuarterHoursDay, 0, 0)
+    }
+    else if((person.startTime.after(sixOCLockMorning) || person.startTime == sixOCLockMorning) && person.startTime.before(sixOClockEvening) && person.endTime.after(sixOClockEvening) && (person.endTime.before(nextDaySixOClockMorning) || person.endTime == nextDaySixOClockMorning)) {
+      val timeDiffInQuarterHoursDay = timeDifferenceInQuarterHours(sixOClockEvening, person.startTime)
+      val timeDiffInQuarterHoursEveningAfterDay = timeDifferenceInQuarterHours(person.endTime, sixOClockEvening)
+
+      PersonByMorningAndEvening(person.id, person.name, beginningOfADay, timeDiffInQuarterHoursDay, 0, timeDiffInQuarterHoursEveningAfterDay)
+    }
+    else if((person.startTime.after(sixOCLockMorning) || person.startTime == sixOCLockMorning) && person.startTime.before(sixOClockEvening) && person.endTime.after(sixOClockEvening) && person.endTime.after(nextDaySixOClockMorning)) {
+      val timeDiffInQuarterHoursDay = timeDifferenceInQuarterHours(sixOClockEvening, person.startTime) + timeDifferenceInQuarterHours(person.endTime, nextDaySixOClockMorning)
+      val timeDiffInQuarterHoursEveningAfterDay = timeDifferenceInQuarterHours(nextDaySixOClockMorning, sixOClockEvening)
+
+      PersonByMorningAndEvening(person.id, person.name, beginningOfADay, timeDiffInQuarterHoursDay, 0, timeDiffInQuarterHoursEveningAfterDay)
+    }
+    else if((person.startTime.after(sixOClockEvening) || person.startTime == sixOClockEvening) && (person.endTime.before(nextDaySixOClockMorning) || person.endTime == nextDaySixOClockMorning)) {
+      val timeDiffInQuarterHoursEveningAfterDay = timeDifferenceInQuarterHours(person.endTime, person.startTime)
+
+      PersonByMorningAndEvening(person.id, person.name, beginningOfADay, 0, 0, timeDiffInQuarterHoursEveningAfterDay)
+    }
+    else if((person.startTime.after(sixOClockEvening) || person.startTime == sixOClockEvening) && person.endTime.after(nextDaySixOClockMorning) && (person.endTime.before(nextDaySixOClockEvening) || person.endTime == nextDaySixOClockEvening)) {
+      val timeDiffInQuarterHoursDay = timeDifferenceInQuarterHours(person.endTime, nextDaySixOClockMorning)
+      val timeDiffInQuarterHoursEveningBeforeDay = timeDifferenceInQuarterHours(nextDaySixOClockMorning, person.startTime)
+
+      PersonByMorningAndEvening(person.id, person.name, beginningOfADay, timeDiffInQuarterHoursDay, timeDiffInQuarterHoursEveningBeforeDay, 0)
+    }
+    else if((person.startTime.after(sixOClockEvening) || person.startTime == sixOClockEvening) && person.endTime.after(nextDaySixOClockEvening)) {
+      val timeDiffInQuarterHoursDay = timeDifferenceInQuarterHours(person.endTime, nextDaySixOClockMorning)
+      val timeDiffInQuarterHoursEveningBeforeDay = timeDifferenceInQuarterHours(nextDaySixOClockMorning, person.startTime)
+      val timeDiffInQuarterHoursEveningAfterDay = timeDifferenceInQuarterHours(person.endTime, nextDaySixOClockEvening)
+
+      PersonByMorningAndEvening(person.id, person.name, beginningOfADay, timeDiffInQuarterHoursDay, timeDiffInQuarterHoursEveningBeforeDay, timeDiffInQuarterHoursEveningAfterDay)
+    }
+    else {
+      PersonByMorningAndEvening(person.id, person.name, beginningOfADay, 0, 0, 0)
+    }
+  }
+
+  // returns a new Timestamp corresponding to 18:00 of a same day as provided Timestamp
   def getSixOCLock(t: Timestamp, morning: Boolean = true, nextDay: Boolean = false): Timestamp = {
     val six = if(nextDay) new Timestamp(t.getTime + 24*3600*1000) else new Timestamp(t.getTime)
     if (morning) six.setHours(6) else six.setHours(18)
